91d6f1930d34e5a87404105949f5618ff952fe06
lettucemayoandonion <lottwriter@gmail.com>
2023-07-13 13:54:06 -0600

    Added Login and SignUp functionality via React

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+     const [username, setUsername] = useState("");
+     const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://movieflixapi-267bf627ca0c.herokuapp.com/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Login response: ", data);
+      if (data.user) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+        onLoggedIn(data.user, data.token);
+      } else {
+        alert("No such user");
+      }
+    })
+    .catch((e) => {
+      alert("Something went wrong");
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,32 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+
+
+  useEffect(() => {
+      if (!token) {
+        return;
+      }
+      
+      fetch("https://movieflixapi-267bf627ca0c.herokuapp.com/movies", {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+        .then((response) => response.json())
+        .then((movies) => {
+          setMovies(movies)
+        });
+    }, [token]);
 
   useEffect(() => {
     fetch("https://movieflixapi-267bf627ca0c.herokuapp.com/movies")
@@ -25,7 +48,24 @@ export const MainView = () => {
     });
 }, []);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  
+
+if (!user) {
+  return (
+    <>
+      <LoginView onLoggedIn={(user, token) => {
+        setUser(user);
+        setToken(token);
+      }} />
+      or
+      <SignupView />
+    </>
+  );
+}
+
+  
+
+    
 
   if (selectedMovie) {
     return (
@@ -52,6 +92,7 @@ console.log(movies)
           }}
         />
       ))}
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
     </div>
   );
 };
