ad29f8f683516ee857a02a89a2e72551a262677a
lettucemayoandonion <lottwriter@gmail.com>
2023-07-28 19:34:11 -0600

    Bug fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@
 import { createRoot } from 'react-dom/client';
 import "./index.scss";
 
-import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import Container from 'react-bootstrap/Container';
 import "./index.scss";
